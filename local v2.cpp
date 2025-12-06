36361fb0b6071ad68de39809ba62c1f32a26e954
jahnavibangarugari-lang <jahnavibangarugari@gmail.com>
2025-12-06 12:17:55 +0530

    Add files via upload

---
+++ local v2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a=10;
+	printf("\n a value is =%d",a);
+	if(a==10)
+	{
+	 a=20;
+	printf("\n a value is =%d",a);
+    }
+    printf("\n a value is =%d",a);
+	return 0;
+}
