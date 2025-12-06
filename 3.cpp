5e495ac30231e767b22a0a27868282b20ef18e78
jahnavibangarugari-lang <jahnavibangarugari@gmail.com>
2025-12-06 12:05:21 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,16 @@
+//working of assignment operator//
+#include<stdio.h>
+int main()
+{
+	int a=5,c;
+	c=a;
+	printf("c=%d\n",c);
+	c+=a;
+	printf("c+=%d\n",c);
+	c-=a;
+	printf("c-=%d\n",c);
+	c/=a;
+	printf("c/=%d\n",c);
+	c%=a;
+	printf("c%=%d\n",c);
+}
