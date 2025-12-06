194fe8d17185fc5adef89bd99728b6ae9102d69c
jahnavibangarugari-lang <jahnavibangarugari@gmail.com>
2025-12-06 12:06:04 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,16 @@
+//working of arithematic operators//
+#include<stdio.h>
+int main()
+{
+	int a=5,b=8,c;
+	c=a+b;
+	printf("a+b=%d\n",c);
+	c=a-b;
+	printf("a-b=%d\n",c);
+	c=a*b;
+	printf("a*b=%d\n",c);
+	c=a/b;
+	printf("a/b=%d\n",c);
+	c=a%b;
+	printf("a%b=%d\n",c);
+}
