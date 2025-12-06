9fd89d02889c185f0d6936f74722672864a241fd
jahnavibangarugari-lang <jahnavibangarugari@gmail.com>
2025-12-06 12:06:47 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,13 @@
+//working of relation operators//
+#include<stdio.h>
+int main()
+{
+	int a=5,b=5;
+	printf("%d<%d is %d\n",a,b,a<b);
+	printf("%d>%d is %d\n",a,b,a>b);
+	printf("%d=%d is %d\n",a,b,a=b);
+	printf("%d<=%d is %d\n",a,b,a<=b);
+	printf("%d>=%d is %d\n",a,b,a>=b);
+	printf("%d==%d is %d\n",a,b,a==b);
+	printf("%d!=%d is %d\n",a,b,a!=b);
+}
