67ca9568fe4f247e52c5bc3add8e299abae9a3e1
jahnavibangarugari-lang <jahnavibangarugari@gmail.com>
2025-12-06 11:50:02 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+/*write a c program print message*/
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("the add answer is %d",c);
+}
